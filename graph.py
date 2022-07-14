fc45d965d643b34af2b5c5f1de92fe3954d49652
Yoshino Taichi <taichi_yoshino@yse-c.net>
2022-07-14 09:38:42 +0900

    グラフを動かして みた

--- graph.py
+++ graph.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x = np.linspace(-10, 10, 101)
-y = np.exp(-x**2 / 2 ) / np.sqrt(2 * np.pi)
+y = np.exp(-x**2 / 2 ) / np.sqrt(2 * np.pi)  -5
 
 plt.plot(x, y)
 plt.show()
